b7a710fdf3c61757a7ab1ded9c82519f693d5779
isturiz <isturizmauricio@gmail.com>
2024-08-28 23:00:08 -0400

    update(colorscheme): vim-enfocado -> sonokai

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
 return {
-    "wuelnerdotexe/vim-enfocado",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme enfocado]])
-    end,
+  "sainnhe/sonokai",
+  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    -- load the colorscheme here
+    vim.g.sonokai_enable_italic = true
+    vim.cmd([[colorscheme sonokai]])
+  end,
+
 }
+
+-- return {
+--     "wuelnerdotexe/vim-enfocado",
+--     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     config = function()
+--       -- load the colorscheme here
+--       vim.cmd([[colorscheme enfocado]])
+--     end,
+-- }
