3f597451511a698a2303dfaaf9c4b230f40dee07
isturiz <isturizmauricio@gmail.com>
2024-09-01 16:51:26 -0400

    docs: add descriptions to keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -57,7 +57,7 @@ return {
       function()
         require("telescope.builtin").git_bcommits()
       end,
-      desc = "Telescope Git status",
+      desc = "Telescope Git commits",
     },
     {
       "<leader>gb",
