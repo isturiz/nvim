8380301ffe7fa8d1414f023277e06efa32e5ae7f
Mauricio Istúriz <isturizmauricio@gmail.com>
2025-04-25 19:01:08 -0400

    add(gitblame)

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,7 @@
 return {
   "lewis6991/gitsigns.nvim",
   opts = {
-    current_line_blame = true,
+    current_line_blame = false,
     signcolumn = false,
     numhl = true,
     max_file_length = 10000,
