c14a630b226aca24b90004218c78b805f7bf7c81
Mauricio Istúriz <isturizmauricio@gmail.com>
2025-05-31 19:41:34 -0400

    update: minor fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,17 +5,10 @@ return {
   config = function()
     -- load the colorscheme here
     vim.g.sonokai_enable_italic = true
+    vim.g.sonokai_style = 'default'  -- Available styles: 'default', 'atlantis', 'andromeda', 'shusia', 'maia', 'espresso'
+    vim.g.sonokai_disable_italic_comment = false  -- Disable italics in comments
+
     vim.cmd([[colorscheme sonokai]])
   end,
 
 }
-
--- return {
---     "wuelnerdotexe/vim-enfocado",
---     lazy = false, -- make sure we load this during startup if it is your main colorscheme
---     priority = 1000, -- make sure to load this before all the other start plugins
---     config = function()
---       -- load the colorscheme here
---       vim.cmd([[colorscheme enfocado]])
---     end,
--- }
