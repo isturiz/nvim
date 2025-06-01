9eab7b2abeb4e805a0277943b00d23382c9dbd5a
Mauricio Istúriz <isturizmauricio@gmail.com>
2025-05-31 21:07:32 -0400

    update(avante): mapping actions (wip, testing still)

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,11 +2,106 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
+  config = function(_, o)
+    require("avante").setup(o)
+    require("meta-plugins.avante.recipes")
+  end,
   opts = {
+    -- this is a test
     provider = "copilot",
-  },
-  file_selector = {
-    provider = "snacks",
+    -- copilot = {
+    --   model = "claude-3.7-sonnet"
+    -- },
+    -- cursor_applying_provider = "copilot",
+    -- auto_suggestions_provider = "copilot",
+    -- behaviour = {
+    --   auto_suggestions = true,               -- Experimental stage
+    --   auto_set_highlight_group = true,
+    --   auto_set_keymaps = true,
+    --   auto_apply_diff_after_generation = false,
+    --   support_paste_from_clipboard = false,
+    --   enable_cursor_planning_mode = true,   -- enable cursor planning mode!
+    -- },
+    --- @alias FileSelectorProvider "native" | "fzf" | "mini.pick" | "snacks" | "telescope" | string
+    file_selector = {
+      provider = "snacks",
+    },
+    mappings = {
+      --- @class AvanteConflictMappings
+      diff = {
+        ours = "co",
+        theirs = "ct",
+        all_theirs = "ca",
+        both = "cb",
+        cursor = "cc",
+        next = "]x",
+        prev = "[x",
+      },
+      suggestion = {
+        accept = "<M-l>",
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+      jump = {
+        next = "]]",
+        prev = "[[",
+      },
+      submit = {
+        normal = "<CR>",
+        insert = "<C-s>",
+      },
+      sidebar = {
+        apply_all = "A",
+        apply_cursor = "a",
+        switch_windows = "<Tab>",
+        reverse_switch_windows = "<S-Tab>",
+      },
+    },
+    hints = { enabled = false },
+    windows = {
+      ---@type "right" | "left" | "top" | "bottom" | "smart"
+      position = "smart", -- the position of the sidebar
+      wrap = true,        -- similar to vim.o.wrap
+      width = 50,         -- default % based on available width
+      sidebar_header = {
+        enabled = true,   -- true, false to enable/disable the header
+        align = "center", -- left, center, right for title
+        rounded = false,
+      },
+      input = {
+        prefix = "> ",
+        height = 8, -- Height of the input window in vertical layout
+      },
+      edit = {
+        start_insert = true, -- Start insert mode when opening the edit window
+      },
+      ask = {
+        floating = false,        -- Open the 'AvanteAsk' prompt in a floating window
+        start_insert = true,     -- Start insert mode when opening the ask window
+        ---@type "ours" | "theirs"
+        focus_on_apply = "ours", -- which diff to focus after applying
+      },
+    },
+    highlights = {
+      ---@type AvanteConflictHighlights
+      diff = {
+        current = "DiffText",
+        incoming = "DiffAdd",
+      },
+    },
+    --- @class AvanteConflictUserConfig
+    diff = {
+      autojump = true,
+      ---@type string | fun(): any
+      list_opener = "copen",
+      --- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
+      --- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
+      --- Disable by setting to -1.
+      override_timeoutlen = 500,
+    },
+    system_prompt = "All generated code must be in English. Ensure elegant and quality code."
+
   },
 
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
