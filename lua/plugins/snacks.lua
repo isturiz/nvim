de599008a72d45a8f0e44b97efa8d98f9e17ac4c
Mauricio Istúriz <isturizmauricio@gmail.com>
2025-08-02 22:04:47 -0400

    update(snacks): remove dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -181,12 +181,6 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = {
-      enabled = true,
-      preset = {
-        header = [[ISTÚRIZ]]
-      },
-    },
     indent = { enabled = true },
     input = { enabled = true },
     picker = {
