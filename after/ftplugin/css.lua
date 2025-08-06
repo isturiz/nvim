1c9a65141f8d78f35390d8758d5a227dd5f69a10
Mauricio Istúriz <isturizmauricio@gmail.com>
2025-08-06 13:15:40 -0400

    add(css & scss): specific settigns for css files

---
+++ after/ftplugin/css.lua
@@ -0,0 +1,8 @@
+-- Set indentation to 4 spaces, no tabs
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 4
+
+-- Set text width for max 80 characters
+vim.bo.textwidth = 80
---
+++ after/ftplugin/scss.lua
@@ -0,0 +1,8 @@
+-- set indentation to 4 spaces, no tabs
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 4
+
+-- set text width for max 80 characters
+vim.bo.textwidth = 80
